6f245345803992a8051bc1881f455ac671a05d85
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-06-21 12:59:01 +0100

    add

--- src/components/DesktopImage.tsx
+++ src/components/DesktopImage.tsx
@@ -1,34 +1,87 @@
-import React from 'react'
-import { useState } from "react";
-import { Photos } from '../utils/originalphotos'
-import { TPhotos } from '../utils/thumbnailphotos'
+import React, { useState } from 'react';
+import { Photos } from '../utils/originalphotos';
+import { TPhotos } from '../utils/thumbnailphotos';
+import Close from "../assets/icon-close.svg"
+import Next from "../assets/icon-next.svg"
+import Previous from "../assets/icon-previous.svg"
 
 const DesktopImage: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLightboxOpen, setLightboxOpen] = useState(false);
 
-  const handleClick = (id:number) => {
-    const photo = Photos.find((photo) => photo.id === id);
-    if (photo) {
-      setCurrentIndex(photo.id - 1);
+  const handleClick = (id: number) => {
+    setCurrentIndex(id - 1);
+  };
+
+  const handleOpenLightbox = () => {
+    setLightboxOpen(true);
+  };
+
+  const handleCloseLightbox = () => {
+    setLightboxOpen(false);
+  };
+
+  const handleNextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % Photos.length);
+  };
+
+  const handlePreviousImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + Photos.length) % Photos.length);
+  };
+
+  const handleLightboxClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      handleCloseLightbox();
     }
   };
-    
+
   return (
     <div className="hidden md:flex flex-col w-2/5 h-[27rem]">
-      <img src={Photos[currentIndex].url} alt="" className='w-full h-[20rem] rounded-xl' />
-      <div className="flex h-[5rem] w-full justify-between mt-[2rem]">
-      {TPhotos.map((photo, id) => (
-      <img
-        key={photo.id}
-        src={photo.url}
-        alt={`Product ${photo.id}`}
-        className={`w-[20%] rounded-lg cursor-pointer hover:opacity-50 hover:border-4 hover:border-Orange ${id === currentIndex ? "opacity-50" : ""}`}
-        onClick={() => handleClick(photo.id)}
-      />
-      ))}
-      </div>
-    </div>
-  )
-}
+      <img src={Photos[currentIndex].url} alt="" className="w-full h-[20rem] rounded-xl" onClick={handleOpenLightbox} />
 
-export default DesktopImage
+      <div className="flex h-[5rem] w-full justify-between mt-[2rem]">
+        {TPhotos.map((photo, id) => (
+          <img
+            key={photo.id}
+            src={photo.url}
+            alt={`Product ${photo.id}`}
+            className={`w-[20%] rounded-lg cursor-pointer hover:opacity-50 hover:border-4 hover:border-Orange ${
+              id === currentIndex ? 'opacity-50' : ''
+            }`}
+            onClick={() => handleClick(photo.id)}
+          />
+        ))}
+      </div>
+
+      {isLightboxOpen && (
+        <div className="fixed w-screen h-screen inset-0 flex flex-col items-center justify-center bg-black bg-opacity-75" onClick={handleLightboxClick}>
+          <div className="relative w-2/5 h-4/6">
+            <img src={Previous} alt="" className="absolute top-1/2 -translate-y-1/2 left-0 m-4 cursor-pointer" onClick={handlePreviousImage} />
+
+            <img src={Close} alt="" className="absolute top-0 right-0 m-4 cursor-pointer" onClick={handleCloseLightbox} />
+
+            <img src={Next} alt="" className="absolute top-1/2 -translate-y-1/2 right-0 m-4 cursor-pointer" onClick={handleNextImage} />
+        
+            <img src={Photos[currentIndex].url} alt="" className="w-full h-full rounded-xl" />
+          </div>
+
+          <div className="flex h-[5rem] w-2/5 justify-between mt-[2rem]">
+            {TPhotos.map((photo, id) => (
+              <img
+                key={photo.id}
+                src={photo.url}
+                alt={`Product ${photo.id}`}
+                className={`w-[20%] rounded-lg cursor-pointer hover:opacity-50 hover:border-4 hover:border-Orange ${
+                  id === currentIndex ? 'opacity-50' : ''
+                }`}
+                onClick={() => handleClick(photo.id)}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DesktopImage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,20 +21,21 @@ const Navbar: React.FC = () => {
         <nav className='flex w-full h-16 md:h-24 justify-between px-5 md:px-10 lg:px-14 items-center'>
             
             <div className="md:hidden">
-                    <button
-                        className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
-                        onClick={navClose}
-                    >
-                        {navbar ? (
-                            <img src={Close} width={15} height={15} alt="close icon" />
-                        ) : (
+                <button
+                    className="p-2 rounded-md outline-none focus:border-gray-400 focus:border"
+                    onClick={navClose}
+                >
+                    {navbar ? (
+                        <img src={Close} width={15} height={15} alt="close icon" />
+                    ) : (
+                        <img src={Menu} width={15} height={15} alt="menu icon" />
+                    )}
+                </button>
+            </div>
 
-                            <img src={Menu} width={15} height={15} alt="menu icon" />
-                        )}
-                    </button>
-                </div>
             <div className="flex justify-between md:w-4/5 lg:w-[70%]">
                 <img src={Logo} alt="logo icon" />
+
                 <ul className={`${navbar ? "block h-screen fixed top-16 left-0 right-0 z-10 pl-5 pt-5 w-4/5 font-bold text-xl animate-open-menu" : "hidden"} md:flex justify-between md:w-3/5 bg-White`}>
                     <li><a href="/">Collections</a></li>
                     <li className='py-3 md:py-0'><a href="/">Men</a></li>
@@ -49,6 +50,7 @@ const Navbar: React.FC = () => {
                 <img src={Avatar} alt="avatar image" className='h-8 cursor-pointer' />
             </div>
         </nav>
+        
         <hr className="bg-slate-200 h-px w-11/12 m-auto hidden md:block" />
     </>
   )
